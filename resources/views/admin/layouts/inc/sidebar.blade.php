d28a564238b487757d4a27dfeff8023d813794fa
xmartina <martinsprince126@gmail.com>
2023-11-10 10:33:21 +0100

    fixed

--- resources/views/admin/layouts/inc/sidebar.blade.php
+++ resources/views/admin/layouts/inc/sidebar.blade.php
@@ -16,12 +16,12 @@
                 <span class="pcoded-mtext">{{ trans_choice('module_admission', 2) }}</span>
             </a>
             <ul class="pcoded-submenu">
-                @canany(['application-create', 'application-view'])
+
                     <li class="{{ Request::is('admin/admission/application*') ? 'active' : '' }}"><a href="{{ route('admin.course-form-pin.create') }}" class="">
 {{--                            {{ trans_choice('module_application', 2) }}--}}
                             Create Course Pin
                         </a></li>
-                @endcanany
+
 
                 @canany(['application-create', 'application-view'])
                 <li class="{{ Request::is('admin/admission/application*') ? 'active' : '' }}"><a href="{{ route('admin.application.index') }}" class="">{{ trans_choice('module_application', 2) }}</a></li>
